6923fa4dcd4dbd66fcd01ab46c7358286b02df5e
Reza <RezaSharify1993.rsmi@gmail.com>
2020-12-23 15:02:48 +0330

    change version code to 1.1.7

--- wallet-dependencies/src/main/java/ir/fanap/wallet_dependencies/Dependency.java
+++ wallet-dependencies/src/main/java/ir/fanap/wallet_dependencies/Dependency.java
@@ -1,22 +1,13 @@
 package ir.fanap.wallet_dependencies;
 
-import java.util.function.Consumer;
+
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by R.Sharifi
  * on 22 Dec 2020
  */
 public class Dependency {
-    public static String VERSION = "1.1.4";
 
-    void test() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            Consumer consumer = new Consumer<String>() {
-                @Override
-                public void accept(String o) {
 
-                }
-            };
-        }
-    }
 }
